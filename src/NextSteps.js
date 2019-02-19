2e3bf084b1936fe3c1fa9a6748324800ba99a1f2
Jeremy Nelson <jermnelson@gmail.com>
2019-02-19 13:25:39 -0800

    content additions to next steps

--- src/NextSteps.js
+++ src/NextSteps.js
@@ -27,11 +27,24 @@ const NextSteps = () => (
       <h2 className="App-title">Get involved</h2>
       <ul>
         <li>Join LD4P Slack channel: ld4.slack.com</li>
-        <li>Github repository for the project,
-           <a href="https://github.com/LD4P/sinopia">https://github.com/LD4P/sinopia</a>
+        <li>Main Github repositories for the project:
+          <ul>
+            <li>Technical Documentation and Resource Template Schemas <a href="https://github.com/LD4P/sinopia">https://github.com/LD4P/sinopia</a></li>
+            <li>Sinopia Linked Data Editor <a href="https://github.com/LD4P/sinopia_editor">https://github.com/LD4P/sinopia_editor</a></li>
+            <li>Sinopia Profile Editor <a href="https://github.com/LD4P/sinopia_profile_editor">https://github.com/LD4P/sinopia_profile_editor</a></li>
+            <li>Sinopia Sample Profiles <a href="https://github.com/LD4P/sinopia_sample_profiles">https://github.com/LD4P/sinopia_sample_profiles</a></li>
+            <li>Sinopa Server <a href="https://github.com/LD4P/sinopia_server">https://github.com/LD4P/sinopia_server</a></li>
+          </ul>
         </li>
         <li>Monitor Sinopia Homepage at <a href="http://sinopia.io/">Sinopia.io</a> for
             the latest news.</li>
+      </ul>
+    </div>
+    <div className="App-body">
+      <h2 className="App-title">User Interface</h2>
+      <ul>
+        <li>Sinopia  <a href="https://invis.io/5YNLDND63KF">Persona</a></li>
+        <li>Sinopia <a href="https://invis.io/WDQFD026J9F">Profile Tab</a></li>
       </ul>
     </div>
     <div className="App-navfooter">
